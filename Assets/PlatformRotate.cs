ef9604e6eee06456a0bab89bbcf13d835138b86d
chr8stoph <57600205+cbathon@users.noreply.github.com>
2024-09-09 20:22:40 -0700

    publish

---
+++ Assets/PlatformRotate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRotate : MonoBehaviour { 
+
+    public Vector3 rotateChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (rotateChange * Time.deltaTime); 
+    }
+}
